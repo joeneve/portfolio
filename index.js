56007b824d05875c3b3db424f6439ffbb214b029
Joe Garcia <77760521+joeneve@users.noreply.github.com>
2023-02-07 16:37:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// add a opacity effect when it first open the website, add event listener with load and add a function that does this
+
+document.addEventListener("DOMcontentloaded", ready); 
+
+function ready() {
+    
+}
