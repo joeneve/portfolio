067a5b1e2cab0494cd0773c97a91bb889152a16d
Joe Garcia <77760521+joeneve@users.noreply.github.com>
2023-02-08 16:56:13 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 document.addEventListener("DOMcontentloaded", ready); 
 
 function ready() {
-    
+    //you'll do kind of the quiz app, add a btn id tag to all of the buttons, 
+    //then do a for loop to them, add event listener when you click on it, 
+    //a function gets triggered with 2 parameters, current slide and desired slide, and do just like the opacity animations and you should be good to go
 }
